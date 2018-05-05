ef402b3fdec230976f66fc0b149af6bb6fb30dec
tbricca <tbricca@gmail.com>
2018-05-05 10:04:38 -0700

    starting new version with a computer instead of user input for player2

---
+++ v3.py
@@ -0,0 +1,30 @@
+####### COMPUTER VERSION ########
+
+# print("Rock...")
+# print("Paper...")
+# print("Scissors...")
+
+# player1 = input("Player 1, make your move: ")
+
+
+
+# if player1 == player2:
+#     print("it's a tie!")
+# elif player1 == "rock":
+#     if player2 == "paper":
+#         print("player2 wins!")
+#     elif player2 == "scissors":
+#         print("player1 wins!")
+# elif player1 == "paper":
+#     if player2 == "scissors":
+#         print("player2 wins!")
+#     elif player2 == "rock":
+#         print("player1 wins!")
+# elif player1 == "scissors":
+#     if player2 == "rock":
+#         print("player2 wins!")
+#     elif player2 == "paper":
+#         print("player1 wins!")    
+
+# else:
+#     print("something went wrong")
