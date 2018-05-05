6280a9dafe33bdd124fe2d4261c60bd655749af4
tbricca <tbricca@gmail.com>
2018-05-05 09:52:10 -0700

    finished refactoring, and changed order to make it more efficent because tieing is the most likely outcome

--- v2.py
+++ v2.py
@@ -5,38 +5,24 @@ print("Scissors...")
 player1 = input("Player 1, make your move: ")
 player2 = input("Player 2, make your move: ")
 
-if player1 == "rock":
-    if player2 == "scissors":
-        print("player1 wins!")
-    if player2 == "paper":
-        print("player2 wins!")
-if player1 == "paper":
-    if player2 == "scissors":
-        print("player2 wins!")
-    if player2 == "rock":
-        print("player1 wins!")
-if player1 == "scissors":
-    if player2 == "rock":
-        print("player2 wins!")
-    if player2 == "paper":
-        print("player1 wins!")
 
-
-
-if player1 == "rock" and player2 == "scissors":
-    print("player1 wins!")
-elif player1 == "rock" and player2 == "paper":
-    print("player2 wins!")
-elif player1 == "paper" and player2 == "rock":
-    print("player1 wins!")
-elif player1 == "paper" and player2 == "scissors":
-    print("player2 wins!")
-elif player1 == "scissors" and player2 == "rock":
-    print("player2 wins!")
-elif player1 == "scissors" and player2 == "paper":
-    print("player1 wins!")
-
-elif player1 == player2:
+if player1 == player2:
     print("it's a tie!")
+elif player1 == "rock":
+    if player2 == "paper":
+        print("player2 wins!")
+    elif player2 == "scissors":
+        print("player1 wins!")
+elif player1 == "paper":
+    if player2 == "scissors":
+        print("player2 wins!")
+    elif player2 == "rock":
+        print("player1 wins!")
+elif player1 == "scissors":
+    if player2 == "rock":
+        print("player2 wins!")
+    elif player2 == "paper":
+        print("player1 wins!")    
+
 else:
     print("something went wrong")
